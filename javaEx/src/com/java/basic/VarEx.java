41390413cd2ddbb98d74a16dd71445909ab939b1
llwindy99ll <llwindy99ll@gmail.com>
2020-08-04 12:25:06 +0900

    두번째

---
+++ javaEx/src/com/java/basic/VarEx.java
@@ -0,0 +1,16 @@
+package com.java.basic;
+
+public class VarEx {
+
+	public static void main(String[] args) {
+		int myAge;
+		myAge = 29;
+		
+		int myAge2 =30;
+		System.out.println("My age is "+myAge);
+		System.out.println("My age2 is "+myAge2);
+		
+		myAge = myAge+1;
+		System.out.println("My age is "+ myAge);
+	}
+}
