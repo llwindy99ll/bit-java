0aa0c97483103e8be9ec8813f6f601106e04f670
junga <junga@LAPTOP-HLM709CC>
2020-08-06 10:57:13 +0900

    증감 논리 연습

--- javaEx/src/com/java/basic/OperEx.java
+++ javaEx/src/com/java/basic/OperEx.java
@@ -5,8 +5,25 @@ public class OperEx {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		arithOper();
+		//arithOper();
+		logicalOper();
+	}
+	
+	// 비교 논리 연산
+	public static void logicalOper() {
 		
+		int n =5;
+		boolean b1 = n>0;
+		boolean b2 = n<10;
+		
+		System.out.println("n > 0 and n < 10 ? : "+ (b1&&b2) );
+	
+		b1 = n <= 0;
+		b2 = n >= 10;
+		
+		System.out.println("n <= 0 or n >= 10 ? : "+ (b1||b2) );
+
+		System.out.println("!(n <= 0 or n >= 10) ? : "+ !(b1||b2) );
 	}
 
 	//산술연산자
@@ -21,6 +38,19 @@ public class OperEx {
 		//정확한 실수값을 얻으려면 int -> float 변환 
 		System.out.println("7/3 =>" + (float)a/b);
 		System.out.println("7%3 =>" + a%b);
+		
+		//실수를 0으로 나눌때 : Infinity
+		System.out.println("실수/0 => "+ 4.0/0);
+		
+		// 계산할수 없는 값 : NaN (Not a Number)
+		System.out.println("0.0 / 0.0 =>" + 0.0/0.0);
+		
+		// 유한수인지 확인 
+		System.out.println("유한수인지 확인 : "+ Double.isFinite(5.0/0));
+
+		// NaN인지 확인 
+		System.out.println("NaN 확인 : "+ Double.isNaN(0.0/0));
+		
 		
 	}
 	
